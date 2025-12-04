67698cbe54834ce18e5a318e73b8d9cf2d780342
Hediipor <hediboumaiza2002@gmail.com>
2025-12-05 00:01:44 +0100

    navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isLoginPopupOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openLoginPopup(): void {
+    this.isLoginPopupOpen = true;
+  }
+
+  closeLoginPopup(): void {
+    this.isLoginPopupOpen = false;
   }
 
 }
