3e8e29d80abecb2b458bbb54cd3fe1cd4deeda51
SKNZ69 <skander.mhamdi@etudiant-isi.utm.tn>
2025-12-05 04:01:55 +0100

    tt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ImgSliderComponent } from "../img-slider/img-slider.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  imports: [ImgSliderComponent]
 })
 export class HomeComponent implements OnInit {
 
