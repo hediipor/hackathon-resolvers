6602ce657e9ff9be5061d5a0b7b993d41c222815
cherifkahmed <cherif.k.ahmed@gmail.com>
2025-12-05 02:45:57 +0100

    khedma bnt qhba

--- src/app/img-slider/img-slider.component.ts
+++ src/app/img-slider/img-slider.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Slide {
+  img: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-img-slider',
   templateUrl: './img-slider.component.html',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgSliderComponent implements OnInit {
 
-  constructor() { }
+  slides: Slide[] = [
+    { img: 'assets/linux.png', text: 'linux' },
+    { img: 'assets/gimp.png', text: 'GIMP' },
+    { img: 'assets/obs.png', text: 'OBS Studio' },
+    { img: 'assets/LibreOffice_logo.svg.png', text: 'LibreOffice' }
+  ];
+
+  currentIndex = 0;
 
   ngOnInit(): void {
+    setInterval(() => this.nextSlide(), 5000);
   }
 
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
 }
